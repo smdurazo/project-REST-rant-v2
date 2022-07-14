9fb70cb4f536ad288151f50c10c937725b66f88d
KS Durazo <sdurazo.UNLV@gmail.com>
2022-07-14 14:56:49 -0700

    implemented delete button

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
 // Express Settings
 app.set('views', __dirname + '/views')
@@ -12,6 +13,7 @@ app.use(express.static('public'))
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,48 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+            <div className="row">
+              <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name} />
+              </div>
+              <div className="col-sm-6">
+                <h1>{ data.place.name }</h1>
+                <h2>
+                  Rating
+                </h2>
+                <h3 className="inactive">
+                  Not Rated
+                </h3>
+                <br />
+                <h2>
+                  Description
+                </h2>
+                <h3>
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
+                <h3>
+                  Serving {data.place.cuisines}
+                </h3>
+                <br />
+                <a href={`/places/${data.i}/edit`} className="btn btn-warning">
+                  Edit
+                </a>{` `}
+                <form method="POST" action={`/places/${data.i}?_method=DELETE`}>
+                  <button type="submit" className="btn btn-danger">
+                    Delete
+                  </button>
+                </form>
+              </div>
+            </div>
+            <hr />
+            <h2>Comments</h2>
+            <h3 className="inactive">
+              No comments yet!
+            </h3>
           </main>
         </Def>
     )
