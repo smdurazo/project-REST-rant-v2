341134f86fbdc87f29f99839f1043eab801853ca
KS Durazo <sdurazo.UNLV@gmail.com>
2022-07-14 14:30:29 -0700

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -6,10 +6,6 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
-})
-
 router.post('/', (req, res) => {
     if (!req.body.pic) {
       // Default image if one is not provided
@@ -25,5 +21,23 @@ router.post('/', (req, res) => {
     res.redirect('/places')
 })
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', { place: places[id] })
+    }
+})
+  
+  
 
 module.exports = router
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -3,19 +3,23 @@ const Def = require('../default')
 
 function index (data) {
     let placesFormatted = data.places.map((place) => {
-      return (
-        <div className="col-sm-6">
-          <h2>{place.name}</h2>
-          <p className="text-center">
-            {place.cuisines}
-          </p>
-          <img src={place.pic} alt={place.name} />
-          <p className="text-center">
-            Located in {place.city}, {place.state}.
-          </p>
-        </div>
-      )
-    })
+        return (
+          <div className="col-sm-6">
+            <h2>
+                <a href={`/places/${index}`} >
+                    {place.name}
+                </a>
+            </h2>
+            <p className="text-center">
+              {place.cuisines}
+            </p>
+            <img src={place.pic} alt={place.name} />
+            <p className="text-center">
+              Located in {place.city}, {place.state}
+            </p>
+          </div>
+        )
+      })      
     return (
       <Def>
           <main>
@@ -25,7 +29,7 @@ function index (data) {
               </div>
           </main>
       </Def>
-    )  
-}
+    )
+  }
 
 module.exports = index;
