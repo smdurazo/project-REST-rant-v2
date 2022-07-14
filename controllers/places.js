2a148b5f744fa299a83f866663630c71e660f6bb
KS Durazo <sdurazo.UNLV@gmail.com>
2022-07-14 14:57:47 -0700

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -37,6 +37,21 @@ router.get('/:id', (req, res) => {
       res.render('places/show', { place: places[id], id})
     }
 })
+
+router.delete('/places/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
+  
   
   
 
