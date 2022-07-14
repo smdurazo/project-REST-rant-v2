ad752af57cfabf1dd68aca8efa480616f6815d2b
KS Durazo <sdurazo.UNLV@gmail.com>
2022-07-14 14:19:50 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,8 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 // GET /places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/dintaifung.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/pasta.jpg'
-      }]      
     res.render('places/index', { places })
 })
 
@@ -23,8 +11,19 @@ router.get('/new', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
+
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Din Tai Fung',
+    city: 'Las Vegas',
+    state: 'NV',
+    cuisines: 'Chinese, Dim-Sum',
+    pic: '/images/dintaifung.jpg'
+  }, {
+    name: 'Carbone',
+    city: 'Las Vegas',
+    state: 'NV',
+    cuisines: 'Italian',
+    pic: '/images/pasta.jpg'
+  }]   
