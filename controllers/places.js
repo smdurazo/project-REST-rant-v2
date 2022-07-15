d7f6f3ea04fbc432586dc5d3927e919495019919
KS Durazo <sdurazo.UNLV@gmail.com>
2022-07-14 21:58:57 -0700

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,6 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-// GET /places
 router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
@@ -26,33 +25,56 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
+    let i = Number(req.params.id)
+    if (isNaN(i)) {
+        res.render('error404')
     }
-    else if (!places[id]) {
-      res.render('error404')
+    else if (!places[i]) {
+        res.render('error404')
     }
     else {
-      res.render('places/show', { place: places[id], id})
+        res.render('places/show', { place: places[i], i })
     }
 })
 
-router.delete('/places/:id', (req, res) => {
+router.put('/:id', (req, res) => {
+    res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+    let i = Number(req.params.id)
+    if (isNaN(i)) {
+        res.render('error404')
+    }
+    else if (!places[i]) {
+        res.render('error404')
+    }
+    else { 
+        places.splice(i, 1)
+        res.redirect('/places')
+    }
+})
+
+router.get('/:id/edit', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
-      res.render('error404')
+        res.render('error404')
     }
     else if (!places[id]) {
-      res.render('error404')
+        res.render('error404')
     }
     else {
-      places.splice(id, 1)
-      res.redirect('/places')
+      res.render('places/edit', { place: places[id] })
     }
-  })
-  
-  
-  
+  })  
+
+router.post('/:id/rant', (req, res) => {
+    res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
 
 module.exports = router
